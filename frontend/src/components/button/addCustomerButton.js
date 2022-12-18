e06265b89724b8af6ec05c2bd2d28c0f5bb1325d
UnoMeDD <DominicDuda@gmx.at>
2022-12-18 12:06:49 +0100

    Changed Register Post endpoint

--- frontend/src/components/button/addCustomerButton.js
+++ frontend/src/components/button/addCustomerButton.js
@@ -1,13 +1,15 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
+import {Link} from "react-router-dom";
 
 export default function addCustomerButton() {
     return (
         <Box sx={{ '& button': { m: 1 } }}>
             <div>
-                <Button variant="contained" size="small">
+                <Button variant="contained" size="small"><Link to="/register">
                     Add Customer
+                </Link>
                 </Button>
             </div>
         </Box>
--- frontend/src/context/auth/AuthState.js
+++ frontend/src/context/auth/AuthState.js
@@ -50,7 +50,7 @@ const AuthState = props => {
     };
 
     try {
-      const res = await axios.post('/api/users', formData, config);
+      const res = await axios.post('/api/register', formData, config);
 
       dispatch({
         type: REGISTER_SUCCESS,
