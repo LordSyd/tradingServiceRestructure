616d6c1b5911f02bf56abfe8978359e8563a5555
anas.kambal <kambal.anas@gmail.com>
2022-12-22 00:54:34 +0100

    created buy and sell stock

--- backend/src/main/java/com/beschtee/backend/Controllers/TradingServiceController.java
+++ backend/src/main/java/com/beschtee/backend/Controllers/TradingServiceController.java
@@ -2,15 +2,14 @@ package com.beschtee.backend.Controllers;
 
 
 import com.beschtee.backend.Services.SoapClient;
-import com.beschtee.backend.stub.FindStockQuotesByCompanyName;
-import com.beschtee.backend.stub.FindStockQuotesByCompanyNameResponse;
-import com.beschtee.backend.stub.ObjectFactory;
-import com.beschtee.backend.stub.PublicStockQuote;
+import com.beschtee.backend.stub.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 @RestController
@@ -27,6 +26,30 @@ public class TradingServiceController {
 
         FindStockQuotesByCompanyNameResponse response = soapClient.getStock("https://edu.dedisys.org/ds-finance/ws/TradingService",
                 objectFactory.createFindStockQuotesByCompanyName(type));
+        return response.getReturn();
+    }
+
+    @PostMapping("/api/buyStock")
+    public BigDecimal buyStock(@RequestParam String symbol, @RequestParam int shares) {
+        ObjectFactory objectFactory = new ObjectFactory();
+        Buy type = new Buy();
+        type.setShares(shares);
+        type.setSymbol(symbol);
+
+        BuyResponse response = soapClient.buyStock("https://edu.dedisys.org/ds-finance/ws/TradingService",
+                objectFactory.createBuy(type));
+        return response.getReturn();
+    }
+
+    @PostMapping("/api/sellStock")
+    public BigDecimal sellStock(@RequestParam String symbol, @RequestParam int shares) {
+        ObjectFactory objectFactory = new ObjectFactory();
+        Sell type = new Sell();
+        type.setShares(shares);
+        type.setSymbol(symbol);
+
+        SellResponse response = soapClient.sellStock("https://edu.dedisys.org/ds-finance/ws/TradingService",
+                objectFactory.createSell(type));
         return response.getReturn();
     }
 }
--- backend/src/main/java/com/beschtee/backend/Services/SoapClient.java
+++ backend/src/main/java/com/beschtee/backend/Services/SoapClient.java
@@ -1,6 +1,9 @@
 package com.beschtee.backend.Services;
 
+import com.beschtee.backend.stub.Buy;
+import com.beschtee.backend.stub.BuyResponse;
 import com.beschtee.backend.stub.FindStockQuotesByCompanyNameResponse;
+import com.beschtee.backend.stub.SellResponse;
 import jakarta.xml.bind.JAXBElement;
 import org.springframework.ws.client.core.support.WebServiceGatewaySupport;
 
@@ -8,6 +11,16 @@ public class SoapClient extends WebServiceGatewaySupport {
     public FindStockQuotesByCompanyNameResponse getStock(String url, Object request){
         JAXBElement res = (JAXBElement) getWebServiceTemplate().marshalSendAndReceive(url, request);
         return (FindStockQuotesByCompanyNameResponse) res.getValue();
+    }
+
+    public BuyResponse buyStock(String url, Object request){
+        JAXBElement res = (JAXBElement) getWebServiceTemplate().marshalSendAndReceive(url, request);
+        return (BuyResponse) res.getValue();
+    }
+
+    public SellResponse sellStock(String url, Object request){
+        JAXBElement res = (JAXBElement) getWebServiceTemplate().marshalSendAndReceive(url, request);
+        return (SellResponse) res.getValue();
     }
 
 }
